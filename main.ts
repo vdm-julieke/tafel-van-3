fc46b9c6a08136b72f0db71e98201bdf78488613
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-03-03 09:54:25 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 let Tafel_3 = 0
 basic.forever(function () {
     basic.showNumber(Tafel_3)
-    Tafel_3 += 3
+    if (Tafel_3 < 12) {
+        Tafel_3 += 3
+    } else {
+        Tafel_3 = 0
+    }
 })
